0c73c4063f7d84b669d5b9636059f0c978b19fda
patelraj1278 <patelraj.ps@gmail.com>
2023-05-05 01:05:28 -0400

    Update Code

--- src/main/java/com/example/springhub/kafka/producer/KafkaProducer.java
+++ src/main/java/com/example/springhub/kafka/producer/KafkaProducer.java
@@ -2,28 +2,37 @@ package com.example.springhub.kafka.producer;
 
 import com.example.springhub.kafka.constant.ApplicationConstant;
 import com.example.springhub.kafka.dto.Student;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.CompletableFuture;
+
 @RestController
 @RequestMapping("/produce")
 public class KafkaProducer {
 
+    Logger logger = LoggerFactory.getLogger(KafkaProducer.class);
     @Autowired
     private KafkaTemplate<String, Object> kafkaTemplate;
 
     @PostMapping("/message")
     public String sendMessage(@RequestBody Student message) {
-
-        try {
-            kafkaTemplate.send(ApplicationConstant.TOPIC_NAME, message);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        CompletableFuture<SendResult<String, Object>> future = kafkaTemplate.send(ApplicationConstant.TOPIC_NAME, message);
+        future.whenComplete((result,throwable) -> {
+            if (result != null){
+                logger.info("Result ->"+result.toString());
+            }else if(throwable != null){
+                logger.debug(throwable.getMessage());
+            }
+        });
         return "json message sent succuessfully";
     }
 
